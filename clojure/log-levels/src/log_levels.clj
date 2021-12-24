34af773b1662455615f4b95f0a4f61ed2d346fe0
Al Khawarizmi <ahmadamerhassan@icloud.com>
2021-12-24 22:45:24 +0200

    clj/log-level solution updated

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -1,20 +1,24 @@
 (ns log-levels
   (:require [clojure.string :as str]))
 
+(def levels #{"[ERROR]:" "[WARNING]:" "[INFO]:"})
 (defn message
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
-  )
+  (str/trim (str/join " "
+                      (filter (complement levels)
+                              (str/split s #" ")))))
 
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
   [s]
-  )
+  (let [level (str/lower-case (first (filter levels (str/split s #" "))))]
+    (subs level 1 (- (count level) 2))))
 
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
   [s]
-  )
+  (str (message s) " (" (log-level s) ")"))
